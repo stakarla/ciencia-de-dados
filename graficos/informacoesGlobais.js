6d9ece0ac885e8c487bb80ac48ef3b2197fdd8a0
Karla <105371331+stakarla@users.noreply.github.com>
2024-10-10 10:34:50 -0300

    okkkkk

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,10 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+async function visualizarInformações (){
+    const res = await fetch (url)
+    const dados = await rest.json()
+    const paragrafo = document.createElement('p')
+    paragrafo.innerHTML = 'você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.'
+    
+    const container = document.getElementById ('graficos-container')
+    container.appendChild(paragrafo)
+}
